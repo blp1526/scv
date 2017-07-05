c33251ace8f42144af246648232b68cd66d57d4f
Shingo Kawamura <blp1526@gmail.com>
2017-07-05 22:30:21 +0900

    Add spec

--- main.go
+++ main.go
@@ -12,5 +12,14 @@ func main() {
 		fmt.Printf(msg, argsSize)
 		os.Exit(1)
 	}
-	fmt.Printf("test %s\n", os.Args[1])
+
+	// TODO: This app's binary is MacOS only.
+	//
+	// * Open $HOME/scv.yml
+	//   * then get Web API ACCESS_TOKEN and ACCESS_TOKEN_SECRET
+	//   * then convert os.Args[1] to Web API :id
+	// * Request "GET /somewhere/:id", then get response as JSON format
+	// * Make "open command argument" from JSON
+	// * Run MacOS command "open argument"
+	fmt.Printf("open %s\n", os.Args[1])
 }
