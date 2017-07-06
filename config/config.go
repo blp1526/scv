66ad43227bbdb2e456c747e674ff63fc4b2e1e2d
Shingo Kawamura <blp1526@gmail.com>
2017-07-07 00:05:22 +0900

    Add api.go

--- config/config.go
+++ config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 type Scv struct {
 	AccessToken       string   `json:"access_token"`
 	AccessTokenSecret string   `json:access_token_secret`
@@ -12,5 +17,17 @@ type Server struct {
 	ID   string `json:"id"`
 }
 
-func Load() {
+func Load(filePath string) (*Scv, error) {
+	var scv Scv
+
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return &scv, err
+	}
+
+	err = json.Unmarshal(bytes, &scv)
+	if err != nil {
+		return &scv, err
+	}
+	return &scv, err
 }
