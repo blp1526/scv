85ee5fcf13ffe476ad2b5cc4c7b4f51ca5e0c52e
Shingo Kawamura <blp1526@gmail.com>
2017-07-08 22:41:12 +0900

    Add Response

--- api/api.go
+++ api/api.go
@@ -1,4 +1,11 @@
 package api
 
-// func Request(zone string, id string) (*Response, err) {
-// }
+type Response struct {
+	Host     string `json:Host`
+	Password string `json:"Password"`
+	Port     string `json:Port`
+}
+
+func Request(zoneName string, ServerId string) (*Response, err) {
+	return response, err
+}
--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,9 +1,26 @@
 package cmd
 
-import "os/exec"
+import (
+	"fmt"
+	"os/exec"
 
-func Run(arg string) error {
-	argument := "foo"
-	err := exec.Command("open", argument).Run()
-	return err
+	"github.com/blp1526/scv/api"
+)
+
+func Run(zoneName string, serverName string) error {
+	apiResponse, err := api.Request(zoneName, serverId)
+	vncPath = cmd.vncPath(apiResponse)
+	err := exec.Command("open", vncPath).Run()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func vncPath(apiResponse) string {
+	password = ""
+	host = ""
+	port = ""
+
+	return "// vnc://:%s@%s:%s" + password + host + port
 }
