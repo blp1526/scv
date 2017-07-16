c00a08ba653f7f7677813cb353db8263aaf3bc39
Shingo Kawamura <blp1526@gmail.com>
2017-07-16 21:53:21 +0900

    Remove os.Exit(1) from logger.go

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -8,17 +8,33 @@ import (
 	"github.com/blp1526/scv/logger"
 )
 
-func Run(zoneName string, serverName string) error {
+const version = "0.0.1"
+const expectedArgsSize = 2
+
+func Run(args ...string) (msg string, err error) {
+	argsSize := len(args) - 1
+
+	if argsSize == 0 {
+		msg = fmt.Sprintf("scv version %s", version)
+		return msg, nil
+	} else if argsSize != expectedArgsSize {
+		return msg, fmt.Errorf("Expected arguments size is %d", expectedArgsSize)
+	}
+
 	body := &api.Body{}
-	err := api.Request(body, zoneName, serverName)
+	zoneName := args[1]
+	serverName := args[2]
+	err = api.Request(body, zoneName, serverName)
 	if err != nil {
-		return err
+		return msg, err
 	}
 
 	vncPath := vncPath(body)
 	logger.Debug(fmt.Sprintf("VNC Path: %s", vncPath))
-	err = exec.Command("open", vncPath).Run()
-	return err
+	command := "open"
+	msg = fmt.Sprintf("%s %s", command, vncPath)
+	err = exec.Command(command, vncPath).Run()
+	return msg, err
 }
 
 func vncPath(body *api.Body) string {
