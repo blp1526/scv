a20db2cb99828c4ff89acef1b079d06fb49b1738
Shingo Kawamura <blp1526@gmail.com>
2017-08-06 20:49:30 +0900

    Add logger_test.go

--- logger.go
+++ logger.go
@@ -2,23 +2,14 @@ package scv
 
 import "fmt"
 
-const normal = "\033[m"
-const black = "\033[30m"
-const red = "\033[31m"
-const green = "\033[32m"
-const yellow = "\033[33m"
-const blue = "\033[34m"
-const magenta = "\033[35m"
-const cyan = "\033[36m"
-const lightGray = "\033[37m"
-
 type Logger struct {
 	Verbose bool
 }
 
 func (logger *Logger) Debug(a string) (n int, err error) {
 	if logger.Verbose {
-		n, err = fmt.Printf(format(lightGray, "debug"), a)
+		format, _ := logger.Format("lightGray", "debug")
+		n, err = fmt.Printf(format, a)
 		return n, err
 	} else {
 		return n, err
@@ -32,10 +23,28 @@ func (logger *Logger) Info(a string) (n int, err error) {
 
 // NOTE: (a interface{}) is expected to be string or error.
 func (logger *Logger) Fatal(a interface{}) (n int, err error) {
-	n, err = fmt.Printf(format(red, "fatal"), a)
+	format, _ := logger.Format("red", "fatal")
+	n, err = fmt.Printf(format, a)
 	return n, err
 }
 
-func format(color string, level string) string {
-	return color + level + ": %s" + normal + "\n"
+func (logger *Logger) Format(colorName string, level string) (format string, err error) {
+	ansiColors := map[string]string{
+		"normal":    "\033[m",
+		"black":     "\033[30m",
+		"red":       "\033[31m",
+		"green":     "\033[32m",
+		"yellow":    "\033[33m",
+		"blue":      "\033[34m",
+		"magenta":   "\033[35m",
+		"cyan":      "\033[36m",
+		"lightGray": "\033[37m",
+	}
+	ansiColor, hasKey := ansiColors[colorName]
+	if !hasKey {
+		err = fmt.Errorf("Unsupported colorName")
+		return format, err
+	}
+	format = ansiColor + level + ": %s" + ansiColors["normal"] + "\n"
+	return format, err
 }
---
+++ logger_test.go
@@ -0,0 +1,42 @@
+package scv
+
+import (
+	"testing"
+)
+
+func TestLoggerFormat(t *testing.T) {
+	tests := []struct {
+		colorName string
+		level     string
+		want      string
+		err       bool
+	}{
+		{
+			colorName: "perple",
+			level:     "foo",
+			want:      "",
+			err:       true,
+		},
+		{
+			colorName: "black",
+			level:     "foo",
+			want:      "\033[30mfoo: %s\033[m\n",
+			err:       false,
+		},
+	}
+
+	logger := &Logger{}
+	for _, test := range tests {
+		got, err := logger.Format(test.colorName, test.level)
+		if !test.err && err != nil {
+			t.Fatalf("colorName: %s", test.colorName)
+		}
+		if test.err && err == nil {
+			t.Fatalf("colorName: %s", test.colorName)
+		}
+		if got != test.want {
+			t.Fatalf("colorName: %s, want: %s, got: %s",
+				test.colorName, test.want, got)
+		}
+	}
+}
