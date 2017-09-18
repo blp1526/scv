299f7ef47290b690ced3c7e886ebe2eefe78923d
Shingo Kawamura <blp1526@gmail.com>
2017-09-18 22:35:26 +0900

    Add NewFlagSet

--- cli_test.go
+++ cli_test.go
@@ -58,3 +58,44 @@ func TestCLIVersionFormat(t *testing.T) {
 		}
 	}
 }
+
+func TestCLIRun(t *testing.T) {
+	tests := []struct {
+		spec    string
+		version string
+		args    []string
+		want    string
+		err     bool
+	}{
+		{
+			spec:    "short version",
+			version: "v0.0.5-36-g8c7d97d",
+			args:    []string{"-V"},
+			want:    "scv version 0.0.5, build g8c7d97d",
+			err:     false,
+		},
+		{
+			spec:    "long version",
+			version: "v0.0.5-36-g8c7d97d",
+			args:    []string{"--version"},
+			want:    "scv version 0.0.5, build g8c7d97d",
+			err:     false,
+		},
+	}
+
+	for _, tt := range tests {
+		cli := &CLI{}
+		Version = tt.version
+		got, err := cli.Run(tt.args)
+
+		if tt.err && err == nil {
+			t.Errorf("spec: %s, tt.err: %v, err: %v", tt.spec, tt.err, err)
+		}
+		if !tt.err && err != nil {
+			t.Errorf("spec: %s, tt.err: %v, err: %v", tt.spec, tt.err, err)
+		}
+		if got != tt.want {
+			t.Errorf("spec: %s, got: %v, tt.want: %v", tt.spec, got, tt.want)
+		}
+	}
+}
